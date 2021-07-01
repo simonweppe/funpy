bf74869f69891cfc2c497b2fbaf13b9109cf7a8f
Emma Nuss <emmashienuss@gmail.com>
2021-06-30 17:01:29 -0700

    added sample script for running postprocessing

---
+++ run_postprocess.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import glob
+import pandas as pd 
+import xarray as xr
+
+rundir = 'mono_test_ata'
+fdir = os.path.join('/data2','enuss','funwave_lab_setup',rundir,'output')
+savedir = os.path.join('/data2', 'enuss', 'funwave_lab_setup', rundir, 'postprocessing', 'compiled_output_test')
+if not os.path.exists(savedir):
+	os.makedirs(savedir)
+
+if not os.path.exists(os.path.join(fdir, 'compiled_output_test', 'dep.out')):
+	os.system('cp /data2/enuss/funwave_lab_setup/'+rundir+'/output/dep.out /data2/enuss/funwave_lab_setup/'+rundir+'/postprocessing/compiled_output/')
+
+dx = 0.05
+dy = 0.1 
+dt = 0.2 
+
+output2netcdf(fdir, savedir, dx, dy, dt, 'eta')
+output2netcdf(fdir, savedir, dx, dy, dt, 'u')
+output2netcdf(fdir, savedir, dx, dy, dt, 'v')
+output2netcdf(fdir, savedir, dx, dy, dt, 'mask')
+output2netcdf(fdir, savedir, dx, dy, dt, 'nubrk')
+
+uv2vorticity(savedir)
