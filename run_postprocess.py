58b32a13b9ab0b3963776a713bdd35720398bf3a
emmashie <emmashienuss@gmail.com>
2021-06-30 18:01:35 -0700

    changed file paths in run_postprocess.py

--- run_postprocess.py
+++ run_postprocess.py
@@ -9,11 +9,11 @@ import funpy.postprocess as fp
 
 rundir = 'mono_test_ata'
 fdir = os.path.join('/data2','enuss','funwave_lab_setup',rundir,'output')
-savedir = os.path.join('/data2', 'enuss', 'funwave_lab_setup', rundir, 'postprocessing', 'compiled_output_test')
+savedir = os.path.join('/data2', 'enuss', 'funwave_lab_setup', rundir, 'postprocessing', 'compiled_output')
 if not os.path.exists(savedir):
 	os.makedirs(savedir)
 
-if not os.path.exists(os.path.join(fdir, 'compiled_output_test', 'dep.out')):
+if not os.path.exists(os.path.join(fdir, 'compiled_output', 'dep.out')):
 	os.system('cp /data2/enuss/funwave_lab_setup/'+rundir+'/output/dep.out /data2/enuss/funwave_lab_setup/'+rundir+'/postprocessing/compiled_output/')
 
 dx = 0.05
