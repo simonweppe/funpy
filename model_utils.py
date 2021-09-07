4ccb1555ddf56f916cda15745806ad10198338c7
Emma Nuss <emmashienuss@gmail.com>
2021-09-07 14:01:04 -0700

    changed loading masked variable to use memory efficiently

--- model_utils.py
+++ model_utils.py
@@ -7,12 +7,10 @@ import numpy.ma as ma
 
 def load_masked_variable(fdir, var, varfile, mask, maskfile):
 	mask_ds = xr.open_mfdataset(os.path.join(fdir, maskfile))
-	mask = mask_ds[mask].values
 	var_ds = xr.open_mfdataset(os.path.join(fdir, varfile))
-	var = var_ds[var].values
 	x = np.asarray(var_ds.x)
 	y = np.asarray(var_ds.y)
-	var_masked = ma.masked_where(mask==0, var)
+	var_masked = ma.masked_where(mask_ds[mask].values==0, var_ds[var].values)
 	return var_masked, x, y 
 
 def compute_Hsig(eta, start, end):
